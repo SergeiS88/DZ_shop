488d11b176189acea111caf69d39393fca63ea2f
SergeiS88 <svsubbotin88@mail.ru>
2023-02-18 21:55:38 +0300

    dz_8

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.menu__header').click(function() {
-        $('.menu__header').toggleClass('open-menu');
-        $('.header__nav').toggleClass('open-menu');
-    });
-});
